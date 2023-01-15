d7083f510e5cfe7d30ae91b07c5e0c38e4825fdb
catzilla <catzillaorz@outlook.com>
2023-01-15 21:37:08 +0800

    feat: update after/plugin vim.keymap | remvoe unused keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,4 @@
 local lsp = require('lsp-zero')
-local null_ls = require('null-ls')
 
 lsp.preset('recommended')
 lsp.ensure_installed({
@@ -21,7 +20,28 @@ lsp.configure('sumneko_lua', {
         }
     }
 })
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  --vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
 lsp.setup()
+
+
+vim.diagnostic.config({
+    virtual_text = true,
+})
 
 -- mason-null-ls config
 -- see documentation of null-null-ls for more configuration options!
