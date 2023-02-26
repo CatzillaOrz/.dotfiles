d65cbe34749ae7808262ce3f130be4b6c1f7caec
catzilla <catzillaorz@outlook.com>
2023-02-26 23:28:21 +0800

    feat: update init.lua

--- lua/lanvim/init.lua
+++ lua/lanvim/init.lua
@@ -3,6 +3,6 @@ require('nvim-autopairs').setup()
 require('lanvim.set')
 require('lanvim.remap')
 require('lanvim.packer')
-require('lanvim.dapui')
-require('lanvim.dapkeymap')
+--require('lanvim.dapui')
+--require('lanvim.dapkeymap')
 
