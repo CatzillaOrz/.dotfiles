b8bc2e084b261599279ae0f0409d8adddee4559b
catzilla <catzillaorz@outlook.com>
2023-03-07 13:46:26 +0800

    feat: cpm config with friendly-snip VS-like

--- lua/lanvim/init.lua
+++ lua/lanvim/init.lua
@@ -3,6 +3,7 @@ require('nvim-autopairs').setup()
 require('lanvim.set')
 require('lanvim.remap')
 require('lanvim.packer')
+require('lanvim.cpmconfig')
 --require('lanvim.dapui')
 --require('lanvim.dapkeymap')
 
