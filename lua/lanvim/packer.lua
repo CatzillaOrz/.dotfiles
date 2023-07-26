387d15cb587c9b5834b09bdf8f6f446b0342a33c
catzilla <catzillaorz@outlook.com>
2023-07-26 23:00:13 +0800

    feat: java lsp jdtls

--- lua/lanvim/packer.lua
+++ lua/lanvim/packer.lua
@@ -4,54 +4,60 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use('mbbill/undotree')
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use('mbbill/undotree')
 
-    -- lsp-zero
+  -- lsp-zero
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'mfussenegger/nvim-jdtls'},
+
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    },
+    config = {
+      clone_timeout = 36000000, -- Timeout, in seconds, for git clones
+    },
+
+    -- Mason-null-ls
     use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+      "jose-elias-alvarez/null-ls.nvim",
+      "jayp0521/mason-null-ls.nvim",
+    },
 
+    -- dapui
+    use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } },
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+    use {
+      'mfussenegger/nvim-dap',
+      'theHamsta/nvim-dap-virtual-text'
+    },
 
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-        },
-        config = {
-            clone_timeout = 36000000, -- Timeout, in seconds, for git clones
-        },
+    -- css
+    use {
+      'ap/vim-css-color'
+    },
 
-        -- Mason-null-ls
-        use {
-            "jose-elias-alvarez/null-ls.nvim",
-            "jayp0521/mason-null-ls.nvim",
-        },
-
-        -- dapui 
-        use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} },
-
-        use{
-            'mfussenegger/nvim-dap',
-            'theHamsta/nvim-dap-virtual-text'
-        },
-
-        -- css
-        use {
-          'ap/vim-css-color'
-        },
+    -- java
+    use {
+      'mfussenegger/nvim-jdtls'
     }
+  }
 end
 )
